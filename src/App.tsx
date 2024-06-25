f481316bf220a9a3b95ee5acc5996f4475917c5a
LucasCastello <lucas.castello314@gmail.com>
2024-06-25 15:46:24 -0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import PersonalInfo from './personal-info/PersonalInfo'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <PersonalInfo />
+      <div>top scrolling</div>
+      <div>main part</div>
+      <div>bottom bar</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/personal-info/PersonalInfo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PersonalInfo () {
+    return (
+        <div>personal info</div>
+    )
+}
+
+export default PersonalInfo
