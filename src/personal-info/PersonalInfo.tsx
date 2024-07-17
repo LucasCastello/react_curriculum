991b70cb3de98535c54a4563b85e3a2ef10ff093
LucasCastello <lucas.castello314@gmail.com>
2024-07-17 14:44:22 -0300

    changed alert to antd message

--- src/personal-info/PersonalInfo.tsx
+++ src/personal-info/PersonalInfo.tsx
@@ -1,34 +1,41 @@
-import { Button } from 'antd'
-import { MailFilled, LinkedinFilled, GithubFilled } from '@ant-design/icons'
+import { Button, message } from 'antd'
+import { MailFilled, LinkedinFilled, GithubFilled, DownloadOutlined } from '@ant-design/icons'
 import FaceImage from '../assets/Face.jpeg'
 // import ProfilePicture from './ProfilePicture';
 import Resume from '../assets/LucasCastelloResume.pdf'
 import './PersonalInfo.css'
 
 export default function PersonalInfo () {
+    const [messageApi, contextHolder] = message.useMessage();
+    const displayInfo = () => {
+        messageApi.open({
+            type: 'success',
+            content: 'E-mail copied to clipboard',
+            className: 'custom-class',
+        });
+    };
     const openTab = (url : string) => {window.open(url, "_blank", "noreferrer")}
-    const copyToClipboard = (str : string) => {
-        navigator.clipboard.writeText(str)
-        alert("Copied email to clipboard ")
-    }
     
     return (
         <div className="personal_info">
+            {contextHolder}
             <div className='title'>
                 <span className='name'>Lucas Castello</span>
                 <br/>
                 <span className='role'>Developer</span>
             </div>
             <img src={FaceImage} alt="Profile picture" />
-            {/* <ProfilePicture showFace={true}/> */}
             <div className='contact_buttons'>
                 <div>
-                    <Button type='text' icon={<MailFilled style={{fontSize: '20px'}}/>} onClick={() => copyToClipboard('lucas.castello314@gmail.com')}/>
+                    <Button type='text' icon={<MailFilled style={{fontSize: '20px'}}/>} onClick={() => {navigator.clipboard.writeText('lucas.castello314@gmail.com'); displayInfo()}}/>
                     <Button type='text' icon={<GithubFilled style={{fontSize: '20px'}}/>} onClick={() => openTab('https://www.linkedin.com/in/lucastello/')}/>
                     <Button type='text' icon={<LinkedinFilled style={{fontSize: '20px'}}/>} onClick={() => openTab('https://github.com/LucasCastello')}/>
                 </div>
                 <a href={Resume} download="LucasCastelloResume" target='_blank'>
-                    <Button>Download CV</Button>
+                    <Button className='download_button'>
+                        <span>Download CV</span>
+                        <span><DownloadOutlined /></span>
+                    </Button>
                 </a>
             </div>
         </div>
