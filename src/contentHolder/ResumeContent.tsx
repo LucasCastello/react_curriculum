04c24efa70249444c0a36946d84ee33835ca45b7
LucasCastello <lucas.castello314@gmail.com>
2024-07-23 11:34:04 -0300

    added resume content file

---
+++ src/contentHolder/ResumeContent.tsx
@@ -0,0 +1,38 @@
+export default {
+    tsystems: {
+        title: "Developer I - T-Systems ",
+        date: "Jan 2024 – Jun 2024",
+        // description: "Global leader on IT and comunication services, part of the Deutsch Telekom group",
+        // job: "Developer I (Jan 2024 – Jun 2024)",
+        experience: `Implemented the server and client sides of an Intrusion Detection System for cars, using <strong>C++</strong>.
+        Also developed a way for testing it using Python.`,
+    },
+    mywork: {
+        title: "Full-Stack Web Developer - Mywork (Feb 2022 – Jun 2023)",
+        // description: "Startup focused on HR solutions for small and medium-sized companies",
+        // job: "Full-Stack Web Developer (Feb 2022 – Jun 2023)",
+        experience: `Developed a new initial panel for manager users, using React and Ruby on Rails, among other technologies, which decreased the amount of unecessary requests to the server, as was visible in the AWS CloudWatch metrics graphics. 
+        Also helped on architecting and implementing the new Vacation feature, being the second feature deployed by the company.`,
+    },
+    unicamp: {
+        title: "State University of Campinas (UNICAMP)",
+        job: "Computer Science and Computer Engineering (Mar 2019 - Dec 2023)",
+        experience: "Extra credits on digital images, machine learning, programming paradigms and advanced physics.",
+    },
+    embrapa: {
+        title: "Undergraduate researcher - EMBRAPA (Sep 2021 – Feb 2022)",
+        // description: "State corporation created to develop technologies aimed at agriculture",
+        // job: "Undergraduate researcher (Sep 2021 – Feb 2022)",
+        experience: "Pariticipated on the adapting of the Century simulator for carbon sequestration models, which involved translating it from Fortran to R, and also creating Python scripts to to automate auxiliary tasks, such as adapting data to Century's format.",
+    },
+    eracing: {
+        title: "UNICAMP E-Racing (Mar 2019 - Dec 2021)",
+        // description: "UNICAMP’s Formula SAE eletric team, starting an autonomous project at the time",
+        job: "Autonomous System Vision Leader (Jul 2021 – Dec 2021)",
+        experience: "Led a four member team to optmize the vision pipeline. Framerate increased by 4 times",
+        job2: "Autonomous System Vision Member (Jan 2021 - Jun 2021",
+        experience2: "As a sole member of the Vision division, developed a pipeline for cone’s pose estimation (cones were the track marking) based on a monocular camera, using Python and a few computer vision algorithms. Initial framerate of about 5 FPS, later increased to 20 FPS as said above.",
+        job3: "Autonomous System Telemetry Member",
+        experience3: "Developed the frontend of a web application that served as car telemetry, using React.",
+    },
+}
