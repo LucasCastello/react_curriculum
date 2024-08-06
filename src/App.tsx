ce478cf90d72924d5e4b48dd3840b8cbb0dca228
LucasCastello <lucas.castello314@gmail.com>
2024-08-06 15:09:34 -0300

    displayed changes on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import {Container, Grid } from '@mui/material'
 import PersonalInfo from './personal-info/PersonalInfo'
-import RoutingBar from './rountingBar/RoutingBar'
+import NavBar from './navBar/NavBar'
+import Banner from './banner/Banner'
+import './App.css'
 
 function App() {
-
   return (
-    <Container>
-      <Grid container spacing={3}>
-        <Grid item sm={12} md={3}>
-          <PersonalInfo />
-        </Grid>
-        <Grid item xs>
-          <RoutingBar />
-          <Outlet />
-        </Grid>
-      </Grid>
-    </Container>
+    <>
+      <NavBar />
+      <div className='app'>
+        <Banner />
+      </div>
+    </>
   )
 }
 
